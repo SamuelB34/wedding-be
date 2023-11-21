04f83db8c2526ecbc621ab28fdc29897a7b0b1ad
Samuel Barragan <samuel.barragan34@hotmail.com>
2023-11-20 16:16:25 -0800

    FInish Login

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,51 @@
+import { BaseController } from "../base.controller"
+import { NextFunction, Request, Response } from "express"
+import users from "../../models/users"
+import { LoginDto } from "./dtos/login.dto"
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+export interface LoginJwt {
+	user_id: string
+	user_username: string
+}
+
+class AuthController extends BaseController {
+	public login = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const body: LoginDto = req.body as LoginDto
+			const password: string = body.password
+
+			const user = await users.find({
+				username: body.username,
+				deleted_at: { $exists: false },
+			})
+
+			if (!user.length)
+				return this.respondInvalid(res, `Invalid username or password`)
+
+			const user_password: string = user[0].password
+
+			const check_user_password = await bcrypt.compare(password, user_password)
+
+			if (!check_user_password)
+				return this.respondInvalid(res, `Invalid username or password`)
+
+			const payload: LoginJwt = {
+				user_id: user[0]._id.toString(),
+				user_username: user[0].username,
+			}
+
+			const token = {
+				id: user[0]._id,
+				jwt: jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "28d" }),
+			}
+
+			return this.respondSuccess(res, `Success`, token)
+		} catch (err) {
+			next(err)
+		}
+	}
+}
+
+export default new AuthController()
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,10 @@
-import express from "express";
-
+import express from "express"
+import { dtoValidation } from "../middlewares/dto-validation"
+import { LoginDto } from "../controllers/auth/dtos/login.dto"
+import AuthController from "../controllers/auth/auth.controller"
 
 const router = express.Router()
 
+router.post("/", dtoValidation(LoginDto), AuthController.login)
 
 export default router
